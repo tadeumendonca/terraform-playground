70db4beb914ea3397c9677f424e879d5cea8adb0
Tadeu Mendonça <tadeu.mendonca@outlook.com>
2019-10-07 22:33:31 -0300

    Defining provisoners.

--- my-first-terraform-configuration/my-first-terraform-configuration.tf
+++ my-first-terraform-configuration/my-first-terraform-configuration.tf
@@ -6,10 +6,9 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
-}
-resource "aws_eip" "ip" {
-    vpc = true
-    instance = aws_instance.example.id
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
 
 resource "aws_instance" "another" {
