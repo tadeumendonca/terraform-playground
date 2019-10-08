665233be40d726606672335d5810726d09eec66d
Tadeu Mendonça <tadeu.mendonca@outlook.com>
2019-10-07 22:27:20 -0300

    Creating a second EC2 than will be built in parallel.

--- my-first-terraform-configuration/my-first-terraform-configuration.tf
+++ my-first-terraform-configuration/my-first-terraform-configuration.tf
@@ -11,3 +11,8 @@ resource "aws_eip" "ip" {
     vpc = true
     instance = aws_instance.example.id
 }
+
+resource "aws_instance" "another" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+}
