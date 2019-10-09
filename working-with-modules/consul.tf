f39bc57b985f87ec647f6607db6b0ba66ed0e116
Tadeu Mendonça <tadeu.mendonca@outlook.com>
2019-10-09 17:09:41 -0300

    Modules.

---
+++ working-with-modules/consul.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "0.12.7"
+}
+
+provider "aws" {
+  region     = "us-east-1"
+}
+
+module "consul" {
+  source      = "hashicorp/consul/aws"
+  num_servers = "3"
+}
+
+output "consul_server_asg_name" {
+  value = "${module.consul.asg_name_servers}"
+}
